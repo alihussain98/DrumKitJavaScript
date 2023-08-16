22282c6680718dbfc37c99ff265bda96a442acd0
alihussain98 <66846038+alihussain98@users.noreply.github.com>
2023-08-16 17:46:42 -0500

    Add files via upload
    Final Commit - Project ready (Added Key Press Animation)

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ for (var i = 0; i < lengthOfDrums; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
 document.addEventListener("keydown", function(event) {
     makeSound(event.key);
+    buttonAnimation(buttonInnerHTML);
 });
 
 function makeSound(key) {
@@ -47,4 +49,14 @@ function makeSound(key) {
       console.log(buttonInnerHTML);
       break;
   }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
